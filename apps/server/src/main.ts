18eb9c7aaa6eb230df7d6a5f92c0cb76c11adeeb
Ravi Lamichhane <ravilamichhane30@gmail.com>
2023-10-04 20:26:13 +0545

    added dev scripts and tsconfig

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
+  const port = process.env.PORT || 4000;
+  const host = process.env.HOST || 'localhost';
+  await app.listen(port);
+  console.log(`Application is running on: http://${host}:${port}`);
 }
 bootstrap();
